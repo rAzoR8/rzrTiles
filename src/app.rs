efd664f8510a331b9e3f4a88a93ae06d1a9f1f90
Fabian Wahlster <fwahlster@outlook.com>
2023-03-21 21:13:28 +0100

    add egui_extras

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,5 @@
+use egui_extras::{TableBuilder, Column};
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
